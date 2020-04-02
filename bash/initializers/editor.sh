94e96cdbe17972c86cfd1b25735cf11a12f96da8
John Nunemaker <nunemaker@gmail.com>
2020-04-02 11:05:53 -0400

    Switch from atom to vscode

--- bash/initializers/editor.sh
+++ bash/initializers/editor.sh
@@ -1,6 +1,6 @@
-export EDITOR='atom --new-window --wait'
-export BUNDLER_EDITOR='atom --new-window'
+export EDITOR='code --new-window --wait'
+export BUNDLER_EDITOR='code --new-window'
 export GIT_EDITOR=vi
 
-alias a='atom .'
-alias e='atom .'
+alias a='code .'
+alias e='code .'
